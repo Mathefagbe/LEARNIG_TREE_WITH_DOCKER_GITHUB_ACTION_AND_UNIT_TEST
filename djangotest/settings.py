5e4baf6171e535f87639be35e53e0e8eea54b309
mathefagbe <fagbemi65@gmail.com>
2024-07-27 18:38:50 +0100

    fix database

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'djangotest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 #     'default':{
 #         # 'ENGINE':'django.db.backends.postqresql',
 #         "ENGINE": "django.db.backends.postgresql",
@@ -102,16 +102,16 @@ WSGI_APPLICATION = 'djangotest.wsgi.application'
 
 #     }
 # }
-DATABASES = {    
-   "default": {        
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("POSTGRES_DB"),
-        "USER": config("POSTGRES_USER"),
-        "PASSWORD": config("POSTGRES_PASSWORD"),
-        "HOST": "db",
-        "PORT": 5432,
-    }
-}
+# DATABASES = {    
+#    "default": {        
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("POSTGRES_DB"),
+#         "USER": config("POSTGRES_USER"),
+#         "PASSWORD": config("POSTGRES_PASSWORD"),
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
 
 
 # Password validation
